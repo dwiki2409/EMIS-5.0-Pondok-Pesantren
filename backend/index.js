7cfb3f0446fdfc7062a76057148e3564f09febe9
1217050089 <1217050089@student.uinsgd.ac.id>
2023-12-11 09:12:07 +0700

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,39 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-// const bodyParser = require("body-parser")
-const app = express()
-const Routes = require("./routes/route.js")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path"); // Import the path module
+const app = express();
+const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-// app.use(bodyParser.json({ limit: '10mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-
-app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(express.json({
+    limit: '10mb'
+}));
+app.use(cors());
 
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
+// Serve static files from the "build" directory
+app.use(express.static(path.join(__dirname, "build")));
+
+// Define your API routes
 app.use('/', Routes);
 
+// For any other route, serve the "index.html" file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
+    console.log(`Server started at port no. ${PORT}`);
+});
