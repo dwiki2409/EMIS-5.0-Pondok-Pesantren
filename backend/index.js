03c1d1f22e5aeb4a60b9216569698a22c05349f6
1217050089 <1217050089@student.uinsgd.ac.id>
2023-12-14 17:28:14 +0700

    aa

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path"); // Import the path module
 const app = express();
 const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 dotenv.config();
 
-app.use(express.json({
-    limit: '10mb'
-}));
-app.use(cors());
+app.use(express.json()) // for parsing application/json data in the request body
+app.use(express.urlencoded({
+    extended: true
+})) // for parsing application/x-www-form-urlencoded data in the request bod
+
 
 mongoose
     .connect(process.env.MONGO_URL, {
